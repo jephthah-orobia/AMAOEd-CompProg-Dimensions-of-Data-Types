f4ea255f14934fc88d728edf861130b080988258
jephthah-orobia <jephthah.orobia@gmail.com>
2025-04-19 18:52:36 -0500

    display table header

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,23 @@
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 int main()
 {
+  char c; //TODO: delete this line
+  const int MAXINPUT = 20, STOCKLIMIT=99999;
 
-  cout << "Hello World!" << endl;
-
-    return EXIT_SUCCESS;
+  /* Print Heading */
+  cout << right << setfill('-') << "+" << setw(MAXINPUT+3) << "+" << setw(8) << "+" << endl; // top border
+  cout << setfill(' ') << "|\033[1m" << left << setw(MAXINPUT+2) << " ITEMS" << "|" << setw(7) << " STOCK" << "|" << endl; // content
+  cout << right << setfill('-') << "+" << setw(MAXINPUT+3) << "+" << setw(8) << "+" << endl; // bottom border of the header
+  
+  //TODO: delete the lines after this
+  cout << "Press any key to exit";
+  cin >> c;
+  cin.ignore();
+  return EXIT_SUCCESS;
 }
