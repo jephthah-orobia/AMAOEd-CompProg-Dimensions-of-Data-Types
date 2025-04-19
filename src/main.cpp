e821e68e953ecc6f8cc70571c63b65230a46d7e2
jephthah-orobia <jephthah.orobia@gmail.com>
2025-04-19 18:57:50 -0500

    Display the full table

--- src/main.cpp
+++ src/main.cpp
@@ -19,9 +19,19 @@ int main()
 
   /* Print Heading */
   cout << right << setfill('-') << "+" << setw(MAXINPUT+3) << "+" << setw(8) << "+" << endl; // top border
-  cout << setfill(' ') << "|\033[1m" << left << setw(MAXINPUT+2) << " ITEMS" << "|" << setw(7) << " STOCK" << "|" << endl; // content
+  cout << setfill(' ') << "|\033[1m" << left << setw(MAXINPUT+2) << " ITEMS" << "\033[0m|" << setw(7) << " STOCK" << "|" << endl; // content
   cout << right << setfill('-') << "+" << setw(MAXINPUT+3) << "+" << setw(8) << "+" << endl; // bottom border of the header
   
+  /* Print table rows */
+  cout << left << setfill(' ');
+  cout << "| " << left << setw(MAXINPUT) << "Apples" << " | " << setw(5) << "10" << " |" << endl; // row
+  cout << "| " << left << setw(MAXINPUT) << "Bananas" << " | " << setw(5) << "20" << " |" << endl; // row
+  cout << "| " << left << setw(MAXINPUT) << "Oranges" << " | " << setw(5) << "15" << " |" << endl; // row
+  cout << "| " << left << setw(MAXINPUT) << "Grapes" << " | " << setw(5) << "5" << " |" << endl; // row
+  
+  /* Print bottom border of the table */
+  cout << right << setfill('-') << "+" << setw(MAXINPUT+3) << "+" << setw(8) << "+" << endl; // bottom border of the header
+
   //TODO: delete the lines after this
   cout << "Press any key to exit";
   cin >> c;
