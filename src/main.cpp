7b471128a8f016ae0dfc0725d1baa0d65330d27f
jephthah-orobia <jephthah.orobia@gmail.com>
2025-04-19 21:01:43 -0500

    Display the theatre seats accodring specified row and col

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
 #include <iostream>
 #include <iomanip>
 #include <limits>
@@ -15,11 +16,31 @@ using namespace std;
 
 int main()
 {
-  const int rows = 5, cols = 10;
+  const int rows = 10, cols = 30;
   char seatingChart[rows][cols];
 
+  memset(seatingChart, 'O', sizeof(seatingChart));
+
   while(true){
+    int r, c;
+
+    /* Print Column Labels */
+    cout << "\033[2m   |";
+    for(int col_label = 1; col_label <= cols; col_label++){
+      cout << " " << left << setfill(' ') << setw(2) << col_label;
+    }
+    cout << endl << "---+" << setfill('-') << setw(cols*3) << "-" << endl;
+    cout << setfill(' ') << left;
     
+    for(int row = 1; row<=rows; row++){
+      cout << "\033[2m" << setw(3) << row << "|\033[0m";
+      for(int col = 1; col<=cols; col++){
+        cout << " " << seatingChart[row-1][col-1] << " ";
+      }
+      cout << endl;
+    }
+    cout << "pause";
+    cin >> r;
   }
 
   return EXIT_SUCCESS;
